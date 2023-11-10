181480a4f2c563d022aaec6c2cff21e642e13c9e
subhan143 <kummithasubahan143@gmail.com>
2023-11-10 15:45:18 +0530

    ok

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const ONN=()=>{
+    document.getElementById("bulbon").style.display="flex"
+    document.getElementById("bulbof").style.display="none"
+    document.getElementById("text").innerText="Touch The button to turn ON the light"
+    document.getElementById("mn").style.backgroundColor="black"
+    document.getElementById("text").style.color="white"
+    document.getElementById("bton").style.display="none"
+    document.getElementById("btof").style.display="flex"
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+
+
+
 }
+const OFF=()=>{
+    document.getElementById("bulbon").style.display="none"
+    document.getElementById("bulbof").style.display="flex"
+    document.getElementById("mn").style.backgroundColor="white"
+    document.getElementById("text").style.color="black"
+    document.getElementById("text").innerText="Touch The button to turn OFF the light"
+    document.getElementById("bton").style.display="flex"
+    document.getElementById("btof").style.display="none"
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+
+
+
 }
